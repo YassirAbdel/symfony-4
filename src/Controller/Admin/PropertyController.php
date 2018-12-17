ada685a2b664d9c1b86aa13d567473c91bdd7954
Abdel <yassir.montet70@gmail.com>
2018-12-17 16:24:00 +0100

    V1.2

--- src/Controller/Admin/PropertyController.php
+++ src/Controller/Admin/PropertyController.php
@@ -12,17 +12,22 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Property;
 use App\Repository\PropertyRepository;
+use App\Form\PropertyType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 
 
 
 class PropertyController extends AbstractController {
 
     private $repository;
+    private $em;
 
-    public function __construct(PropertyRepository $repository)
+    public function __construct(PropertyRepository $repository, ObjectManager $em)
     {
 
         $this->repository = $repository;
+        $this->em = $em;
     }
 
      /**
@@ -35,17 +40,73 @@ class PropertyController extends AbstractController {
         $properties = $this->repository->findAll();
         return $this->render('admin/property/index.html.twig', compact('properties'));
     }
-
+    
     /**
-     * @Route("admin/{id}/edit", name="admin.property.edit")
-     * @param $property
-     * @return Response
+     * @Route("admin/property/create", name="admin.property.add")
+     * @param $request
      */
-
-    public function edit(Property $property) : Response
+    
+    public function add(Request $request) 
     {
-        return $this->render("admin/property/edit.html.twig", compact('property'));
+        // on crée une class vide
+        $property = new Property();
+        // on crée un formulaire
+        $form = $this->createForm(PropertyType::class, $property);
+        // on passe la requete
+        $form->handleRequest($request);
+        
+        // est-ce que le requete a été envoyée ? et est-ce que ce formulaire est validé ?
+        if ($form->isSubmitted() && $form->isValid()){
+            // Avant de flasher, on dit à em de persiter
+            $this->em->persist($property);
+            $this->em->flush();
+            $this->addFlash('success', 'Bien créé avec succès');
+            return $this->redirectToRoute('admin.property.index');
+        }
+        return $this->render("admin/property/new.html.twig", [
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
     }
 
+    /**
+     * @Route("admin/property/{id}", name="admin.property.edit", methods="GET|POST")
+     * @param Property $property
+     * @param Request $request
+     */
 
+    public function edit(Property $property, Request $request)
+    {
+        $form = $this->createForm(PropertyType::class, $property);
+        
+        // on passe la requetee
+        $form->handleRequest($request);
+        
+        // est-ce que le requete a été envoyée ? et est-ce que ce formulaire est validé ?
+        if ($form->isSubmitted() && $form->isValid()){
+            // Execution requete BD
+            $this->em->flush();
+            $this->addFlash('success', 'Bien modifié avec succès');
+            return $this->redirectToRoute('admin.property.index');
+        }
+        return $this->render("admin/property/edit.html.twig", [
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+    
+    /**
+     * @Route("admin/property/{id}", name="admin.property.delete", methods="DELETE")
+     * @param Property $property
+    */
+    
+    function delete(Property $property, Request $request) 
+    {
+        if ($this->isCsrfTokenValid('delete' . $property->getId(), $request->get('_token'))){
+            $this->em->remove($property);
+            $this->em->flush();
+            $this->addFlash('success', 'Bien supprimé avec succès');
+        }
+        return $this->redirectToRoute('admin.property.index');
+    }
 }
